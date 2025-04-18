0874c4f59e055e6d2ff3d89182e2b9de3029b115
marcusquinn <6428977+marcusquinn@users.noreply.github.com>
2025-04-18 17:54:07 +0100

    Apply stashed changes after merging test workflow fix

--- wp-plugin-starter-template.php
+++ wp-plugin-starter-template.php
@@ -32,7 +32,7 @@ if (!defined('WPINC')) {
 }
 
 // Load the main plugin class
-require_once plugin_dir_path(__FILE__) . 'includes/plugin.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin.php';
 
 // Initialize the plugin
 new WPALLSTARS\PluginStarterTemplate\Plugin(__FILE__, '0.1.7');
