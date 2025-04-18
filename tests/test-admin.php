bcdbbb454c7cfeac49b3d9a3a5d090733127d7ec
marcusquinn <6428977+marcusquinn@users.noreply.github.com>
2025-04-18 18:25:03 +0100

    Revert: wp_mock to 0.3.0 and remove :void from AdminTest::setUp

--- tests/test-admin.php
+++ tests/test-admin.php
@@ -30,7 +30,7 @@ class AdminTest extends \WP_Mock\Tools\TestCase {
     /**
      * Set up test environment
      */
-    public function setUp(): void {
+    public function setUp() {
         parent::setUp();
 
         // Set up mocks
