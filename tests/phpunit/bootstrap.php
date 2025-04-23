ca5a9cf38b546e67f0d1b4c94203b13262610439
marcusquinn <6428977+marcusquinn@users.noreply.github.com>
2025-04-23 12:42:13 +0100

    Fix PHPUnit tests and SonarCloud analysis

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -8,20 +8,43 @@
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available.
 require_once dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php';
 
-// Give access to tests_add_filter() function.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+// Check if we're running the WordPress tests
+if ( getenv( 'WP_PHPUNIT__DIR' ) ) {
+	// Give access to tests_add_filter() function.
+	require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __DIR__ ) ) . '/wp-plugin-starter-template.php';
-	// Load the multisite class for testing
-	$multisite_file = dirname( dirname( __DIR__ ) ) . '/includes/multisite/class-multisite.php';
-	if (file_exists($multisite_file)) {
-		require $multisite_file;
+	/**
+	 * Manually load the plugin being tested.
+	 */
+	function _manually_load_plugin() {
+		require dirname( dirname( __DIR__ ) ) . '/wp-plugin-starter-template.php';
+		// Load the multisite class for testing
+		$multisite_file = dirname( dirname( __DIR__ ) ) . '/includes/multisite/class-multisite.php';
+		if (file_exists($multisite_file)) {
+			require $multisite_file;
+		}
+	}
+
+	// Start up the WP testing environment.
+	require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
+} else {
+	// We're running the WP_Mock tests
+	WP_Mock::bootstrap();
+
+	/**
+	 * Now we define a few constants to help us with testing.
+	 */
+	define('WPST_PLUGIN_DIR', dirname(dirname(__DIR__)) . '/');
+	define('WPST_PLUGIN_URL', 'http://example.org/wp-content/plugins/wp-plugin-starter-template/');
+	define('WPST_VERSION', '0.1.0');
+
+	/**
+	 * Now we include any plugin files that we need to be able to run the tests.
+	 * This should be files that define the functions and classes you're going to test.
+	 */
+	require_once WPST_PLUGIN_DIR . 'includes/class-core.php';
+	require_once WPST_PLUGIN_DIR . 'includes/class-plugin.php';
+	if (file_exists(WPST_PLUGIN_DIR . 'admin/lib/admin.php')) {
+		require_once WPST_PLUGIN_DIR . 'admin/lib/admin.php';
 	}
 }
-
-// Start up the WP testing environment.
-require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
